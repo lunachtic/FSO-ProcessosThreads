7683dca0a75170324aeed319fb2cee81ffd1d546
Lucas Gerlach Nachtigall <96704627+lunachtic@users.noreply.github.com>
2023-06-04 10:59:58 -0300

    Create main.c

---
+++ Processos/main.c
@@ -0,0 +1,15 @@
+/* main.c */
+/* 0 copyright/licensing */
+/* 1 includes */
+#include <stdio.h>
+/* 2 defines */
+/* 3 external declarations */
+/* 4 typedefs */
+/* 5 global variable declarations */
+/* 6 function prototypes */
+
+int main(int argc, char *argv[]) {
+/* 7 command-line parsing */
+}
+
+/* 8 function declarations */
