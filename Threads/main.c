73351e1e800b83aa34d19537ae8faf5f47ce45d6
Lucas Gerlach Nachtigall <96704627+lunachtic@users.noreply.github.com>
2023-06-27 13:39:50 -0300

    Update main.c

--- Threads/main.c
+++ Threads/main.c
@@ -14,19 +14,13 @@ int main() {
     pthread_t threads[NUM_THREADS];
     int thread_args[NUM_THREADS];
     int i;
-
     // Create threads
     for (i = 0; i < NUM_THREADS; i++) {
         thread_args[i] = i;
         pthread_create(&threads[i], NULL, thread_function, &thread_args[i]);
     }
-
     // Wait for threads to finish
-    for (i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
+    for (i = 0; i < NUM_THREADS; i++) {pthread_join(threads[i], NULL);}
     printf("All threads have finished execution.\n");
-
     return 0;
 }
